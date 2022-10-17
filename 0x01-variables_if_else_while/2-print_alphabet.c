2d7ebc9ae0f62d20033836aef15577b138ad45df
johnsonomonu <johnsonomonu144@gmail.com>
2022-10-17 16:07:49 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,21 +6,15 @@
  */
 
 int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 
 {
 
-	char ch;
+putchar(ch);
+}
+putchar('\n');
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-
-	{
-
-		putchar(ch);
-
-	}
-
-	putchar('\n');
-
-	return (0);
-
+return (0);
 }
